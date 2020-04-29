fe99962492742f0b8977398a747b7fbd59d90a0e
Flotzdrue <flotzflotz6@gmail.com>
2020-04-29 14:32:20 +0200

    models wrapper

--- nldenet/machinelearning/models/_debug_model.py
+++ nldenet/machinelearning/models/_debug_model.py
@@ -9,21 +9,22 @@ this is a sample model for code style references
 '''
 
 # local imports
-
-
-# standard lib imports
-
+from nldenet.utils.logger import global_logger as global_logger
 
 # 3rd party imports
 import tensorflow as tf
 import numpy as np
 
 def debug_model(x, w, b, id=0):
-    with tf.variable_scope('debug_model_{}'.format(id), reuse=tf.AUTO_REUSE):
-        net = tf.linalg.transpose(x)
-        net = tf.linalg.matmul(net, w)
-        net = tf.math.add(net, b)
-        net = tf.math.sigmoid(net)
+    '''
+    a simple example model
+
+    this function could also contain shape checks for inputs, to then give relvant debug information
+    '''
+    net = tf.linalg.transpose(x)
+    net = tf.linalg.matmul(net, w)
+    net = tf.math.add(net, b)
+    net = tf.math.sigmoid(net)
 
     return net
 
