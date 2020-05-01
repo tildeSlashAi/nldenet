5dafd925208a4d64efe4ba965e2698b4d9dc9580
Flotzdrue <flotzflotz6@gmail.com>
2020-05-01 23:32:26 +0200

    MultiSequential

--- nldenet/machinelearning/models/__init__.py
+++ nldenet/machinelearning/models/__init__.py
@@ -11,6 +11,3 @@ this file wraps all models in functions for easy importing
 # local imports
 from nldenet.machinelearning.models._model_class import SampleModel
 from nldenet.machinelearning.models._multi_sequential import MultiSequential
-
-#def debug_model(x, w, b, id=0):
-#    return nldenet.machinelearning.models._debug_model.debug_model(x, w, b, id)
--- nldenet/machinelearning/models/_multi_sequential.py
+++ nldenet/machinelearning/models/_multi_sequential.py
@@ -10,5 +10,35 @@ this file wraps all models in functions for easy importing
 
 #local imports
 from nldenet.utils.config import global_config as global_config
-from nldenet.utils.logger import global_logger as global_config
+from nldenet.utils.logger import global_logger as global_logger
+from nldenet.machinelearning.models._model_class import ModelClass
 
+# 3rd party imports
+import tensorflow as tf
+import numpy as np
+
+class MultiSequential(ModelClass):
+    '''
+    Multi Sequential Model for fitting solutions to nonlinear differential equation solutions
+
+    flagship network architecture of nldenet (NonLinear Differential Equation net)
+
+    Parallel feedforward networks with backwards skip connections to adjacent feedforward branch.
+    '''
+
+    def __init__(self, input):
+        '''
+        check input shape
+        '''
+        super().__init__()
+        self._net = tf.linalg.transpose(input)
+
+
+    def build(self):
+        '''
+        conduct shape match checks ahead of runtime
+
+        returns: tensorflow computational graph
+        '''
+        # run shape match checks
+        return self._net
