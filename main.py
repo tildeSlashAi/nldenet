db0edacc0e4036cec8133e63ad7c1726e4260378
Flotzdrue <flotzflotz6@gmail.com>
2020-05-02 19:34:45 +0200

    -

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #-----------------------------------------------------
-# nldenet (c) 2019 Dominique F. Garmier MIT licence
+# nldenet (c) 2020 Dominique F. Garmier MIT licence
 #-----------------------------------------------------
 
 import tensorflow as tf
