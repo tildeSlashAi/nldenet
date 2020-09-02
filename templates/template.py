c812c0facaaee61b23e33561ea3690df2bc72c01
Dominique <dominique@garmier.ch>
2020-09-02 13:23:26 +0200

    added a template for a python file header

---
+++ templates/template.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------
+# (c) 2020 tildeSlashAi Team, All Rights Reserved.
+# Licensed under the MIT License
+#
+# tildeSlashAi Team:
+#   - Dominique F. Garmier
+# ----------------------------------------------------
